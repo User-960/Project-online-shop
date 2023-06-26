385e405049f134efffcca0c209c1e60bcc3e650f
Ivan <ivanvolodin960@gmail.com>
2023-06-26 15:08:38 +0300

    feat: add logics and styles for component ProductItem

--- app/store/store.ts
+++ app/store/store.ts
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit/dist/configureStore";
+import { configureStore } from "@reduxjs/toolkit";
 import { productApi } from "./product/product.api";
 
 export const store = configureStore({
