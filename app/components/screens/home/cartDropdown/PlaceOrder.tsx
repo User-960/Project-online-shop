67f742486b7cd78718b7a7b468d72b84b1035830
Ivan <ivanvolodin960@gmail.com>
2023-06-27 15:29:21 +0300

    feat: create component PlaceOrder

--- app/components/screens/home/cartDropdown/PlaceOrder.tsx
+++ app/components/screens/home/cartDropdown/PlaceOrder.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link';
 import React from 'react';
 
-const PlaceOrder = () => {
+const PlaceOrder: React.FC = () => {
   return (
-    <div>
-      Place Order
-    </div>
+    <Link href='checkout'>
+      <p className='bg-green-800 shadow-md shadow-green-100 rounded-xl block text-center p-3 text-white w-3/5 mx-auto mt-5 hover:bg-green-700 duration-300 transition-colors ease-in'>
+        Place order
+      </p>
+    </Link>
   )
 }
 
